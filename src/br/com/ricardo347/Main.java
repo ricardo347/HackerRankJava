18ade522d8dada3a3950ee52fbfc2f7aa54d3d5b
ricardo347 <ricardo347@msn.com>
2020-01-06 12:15:05 -0300

    Commit RepeatedString advanced solution Done!

--- src/br/com/ricardo347/Main.java
+++ src/br/com/ricardo347/Main.java
@@ -8,8 +8,13 @@ import br.com.ricardo347.RepeatedString.Pattern;
 public class Main {
 
     public static void main(String[] args) {
+        String s = "teste";
+        String a = s.substring(0,0);
+
+
         Pattern p = new Pattern();
-        p.play(p.generator(),10);
+        p.play(p.generator(),736778906400L);
+
 
     }
 }
--- src/br/com/ricardo347/RepeatedString/Pattern.java
+++ src/br/com/ricardo347/RepeatedString/Pattern.java
@@ -1,30 +1,30 @@
 package br.com.ricardo347.RepeatedString;
 
-
-
 public class Pattern {
+
     public Pattern(){
 
     }
 
     public String generator(){
-        return "aba";
+        return "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm";
     }
 
-    public int play(String s, long n){
-       long fault = n % s.length();
-       StringBuilder sb = new StringBuilder();
-       sb.append(s);
+    public long play(String s, long n){
+       Long fault = n % s.length();
+       String tail = s.substring(0, Integer.valueOf(fault.intValue()) );
 
        int count = 0;
+       int tailCount = 0;
 
        for(int i = 0; i < s.length(); i++){
           if(s.charAt(i) == 'a')
-              count ++;
+              count++;
+          if(i < tail.length() && tail.charAt(i) == 'a')
+              tailCount++;
        }
 
-       System.out.println(fault);
-       System.out.println(1000);
-       return 0;
+       System.out.println ((n / s.length() * count) + tailCount);
+       return  (n / s.length() * count) + tailCount;
     }
 }
