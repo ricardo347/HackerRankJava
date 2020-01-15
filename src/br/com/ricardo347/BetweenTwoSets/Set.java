28f0a86e07f6025744c54e83d35674b2a358191f
ricardo347 <ricardo347@msn.com>
2020-01-15 20:30:33 -0300

    Between two Sets Done, recursive approach

---
+++ src/br/com/ricardo347/BetweenTwoSets/Set.java
@@ -0,0 +1,40 @@
+package br.com.ricardo347.BetweenTwoSets;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Set {
+    public Set(){
+
+    }
+
+    public int defaultTestCase(){
+        return 0;
+    }
+
+    public int play(List<Integer> a, List<Integer> b){
+        List<Integer> results = new ArrayList<>();
+        int count = 0;
+        //b.get(0) contains the MIN value for a factor, once arrays are sorted, no other value will be
+        //greater than MIN factor of b.
+        for(int i = b.get(0); i > 0; i--){
+          if(getFactoriesB(0, i, b ) && getFactoriesA(0, i, a ))
+            System.out.printf("%d ",i);
+            count++;
+        }
+        return count;
+    }
+
+    public boolean getFactoriesB(int next, int factor, List<Integer> arr){
+        if(next == arr.size())
+            return true;
+        return arr.get(next) % factor == 0 && getFactoriesB(next + 1, factor, arr);
+    }
+    public boolean getFactoriesA(int next, int factor, List<Integer> arr){
+        if(next == arr.size())
+            return true;
+        return factor % arr.get(next) == 0 && getFactoriesA(next + 1, factor, arr);
+    }
+
+}
--- src/br/com/ricardo347/Main.java
+++ src/br/com/ricardo347/Main.java
@@ -2,17 +2,30 @@ package br.com.ricardo347;
 
 
 import br.com.ricardo347.AssesmentPuzzle.TreasureTruckPop.Parking;
+import br.com.ricardo347.BetweenTwoSets.Set;
 import br.com.ricardo347.BirthdayChocolate.Bar;
 import br.com.ricardo347.Java1DGame.Game1D;
 import br.com.ricardo347.Subarray.Subarray;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    public static void main (String[] args) {
-        
+    public static void main(String[] args) {
+        Set s = new Set();
+        List<Integer> a = new ArrayList<>();
+        a.add(1);
+
+        List<Integer> b = new ArrayList<>();
+        b.add(100);
+
+
+
+        s.play(a, b);
 
     }
+
+
 }
